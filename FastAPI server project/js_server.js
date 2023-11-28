2bc917bad224efe68a8f65b2a864c0e17c8ab843
Sahas <82393353+SSahas@users.noreply.github.com>
2023-11-28 19:44:01 +0530

    Update js_server.js
    produce stream output in terminal

--- FastAPI server project/js_server.js
+++ FastAPI server project/js_server.js
@@ -1,52 +1,57 @@
 const express = require('express');
-
 const axios = require('axios');
-
 const app = express();
 
 let data = JSON.stringify({
-  "query": "Hello, I am sahas. i am here for an interview.",
-  "stream": "True"
+    "query": "Hello, I am sahas. i am here for an interview.",
+    "stream": "True"
 });
 
 let config = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: 'http://localhost:8000/question',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer asmamMSKksaksapSKmskasssmlkappi'
-  },
-  data: data,
-  responseType: 'stream'  // Set the responseType to 'stream'
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:8000/question',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer asmamMSKksaksapSKmskasssmlkappi'
+    },
+    data: data,
+    responseType: 'stream'
 };
 
 app.use(express.json());
 app.get('/get-data', async (req, res) => {
-
     res.set("Content-Type", "text/event-stream");
-  axios.request(config)
-    .then((response) => {
-      // Log each chunk of data in the stream
-      
-      response.data.on('data', (chunk) => {
-        console.log(chunk.toString());  // Assuming the data is in string format
-        res.write(chunk.toString())
-      });
 
-      // Handle the end of the stream
-      response.data.on('end', () => {
-        console.log('Stream ended');
-      });
-    })
-    .catch((error) => {
-      console.error(error);
+
+    let full = "";
+    const response = await axios.request(config);
+
+    // Log data chunks
+    response.data.on('data', (chunk) => {
+        
+        const eventData = chunk.toString().split('\n');
+        eventData.forEach((event) => {
+            if (event.startsWith('data:')) {
+                const dataValue = event.replace('data: ', '');
+                full += JSON.stringify(`${dataValue}`);
+                console.clear();
+                console.log(full);
+                res.write(`data: ${dataValue}\n\n`);
+            }
+        });
     });
 
-})
+    // Handle the end of the stream
+    response.data.on('end', () => {
+        console.log('Stream ended');
+        //res.end();
+    });
+
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Node.js server is running on port ${port}`);
+    console.log(`Node.js server is running on port ${port}`);
 });
